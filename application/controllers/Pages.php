f30fb656bff8063136697c1c09ea65967b49781a
tima-t <tito.marchev@gmail.com>
2016-03-16 20:18:36 +0200

    codeigniter demo news-app Please enter the commit message for your changes. Lines starting

---
+++ application/controllers/Pages.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: Tito
+ * Date: 3/16/2016
+ * Time: 6:53 PM
+ */
+class Pages extends CI_Controller {
+
+
+        public function view($page = 'home')
+        {
+         if ( ! file_exists(APPPATH.'/views/pages/'.$page.'.php'))
+         {
+            // Whoops, we don't have a page for that!
+            show_404();
+         }
+
+            $data['title'] = ucfirst($page); // Capitalize the first letter
+
+            $this->load->view('header', $data);
+            $this->load->view('pages/'.$page, $data);
+            $this->load->view('footer', $data);
+        }
+
+
+
+}
---
+++ application/views/news/success.php
@@ -0,0 +1,1 @@
+<p> your news are submited</p>
